5d65e5735110ccc31a40e3c7d1fedc8473743281
nasirgitweb <freelancernasir46@gmail.com>
2024-04-16 14:06:07 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+    strings: ["Gazi Nasir Ahamed"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backdelay: 1000,
+    loop: true
+});
+var typed = new Typed(".typingg", {
+    strings: ["Digital Marketer","Web Developer","Software Engineer","Freelancer"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backdelay: 1000,
+    loop: true
+});
